4c994d871a100ba86adc9ee10e3ed306baaac31b
Jamil Weeks <jamilweeks@yahoo.com>
2023-09-21 15:13:21 -0400

    aligned navbar and main text with css

--- new-weeks-blog/app/layout.tsx
+++ new-weeks-blog/app/layout.tsx
@@ -19,8 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={` ${inter.className} bg-white text-black dark:bg-gray-900 dark:seletion::bg-gray900 dark:text-whhite h-full selection:bg-gray`}>
         <Navbar/>
-        {children}
-        
+        <main className='max-w-3xl mx-auto px-4 sm:px-6 lg:px-8'>{children}</main>  
         </body>
     </html>
   )
