e89aae2a4c0d350ecbbf5067ded0799fa3f1e9b5
Jamil Weeks <jamilweeks@yahoo.com>
2023-09-21 16:09:51 -0400

    added theme button component

--- new-weeks-blog/app/components/Providers.tsx
+++ new-weeks-blog/app/components/Providers.tsx
@@ -3,7 +3,7 @@
 import { ReactNode } from "react";
 import { ThemeProvider } from "next-themes"
 
-export function Providers( {children}: {children: ReactNode})   {
+export function Providers( { children }: { children: ReactNode })   {
 
     return <ThemeProvider attribute="class">{children}</ThemeProvider>
 }
